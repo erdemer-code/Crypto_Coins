78f752cdbe2297e8ef5a59e53cda300d87a6cbf7
atuncer <atahant99@gmail.com>
2021-11-20 23:04:32 +0300

    Pushing the files didn't got pushed in the previous commit

---
+++ app/src/main/java/com/ozu/cs394/cryptocoins/ui/adapter/RecyclerAdapter.kt
@@ -0,0 +1,44 @@
+package com.ozu.cs394.cryptocoins.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ozu.cs394.cryptocoins.R
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
+
+    private var titles = arrayOf("one", "two", "three","one", "two", "three","one", "two", "three")
+    private var details = arrayOf("details1", "details2", "details3","details1", "details2", "details3","details1", "details2", "details3")
+    private var images = intArrayOf(R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        holder.itemTitle.text = titles[position]
+        holder.itemDetail.text = details[position]
+        holder.itemImage.setImageResource(images[position])
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init{
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemDetail = itemView.findViewById(R.id.item_detail)
+        }
+
+    }
+}
