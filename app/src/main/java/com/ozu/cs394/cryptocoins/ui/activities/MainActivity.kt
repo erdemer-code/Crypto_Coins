f84a73b4e4581092104b3b2b016d66594dc70d28
yigitistanbullu <yigit.istanbullu@ozu.edu.tr>
2021-12-20 20:03:53 +0300

    Firebase Config

--- app/src/main/java/com/ozu/cs394/cryptocoins/ui/activities/MainActivity.kt
+++ app/src/main/java/com/ozu/cs394/cryptocoins/ui/activities/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ozu.cs394.cryptocoins.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.findNavController
 import com.ozu.cs394.cryptocoins.R
 
@@ -12,6 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportFragmentManager.findFragmentById(R.id.nav_graph)
         setContentView(R.layout.activity_main)
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
+
 
     }
 
